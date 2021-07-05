36d6f69aca28dfc45f77f2be83312cb9dc6963ec
ochux <ochux007@gmail.com>
2021-07-05 10:35:42 +0100

    uvwie_language_app

--- app/src/main/java/com/example/to_do/LanguageActivity.kt
+++ app/src/main/java/com/example/to_do/LanguageActivity.kt
@@ -1,11 +1,18 @@
 package com.example.to_do
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LanguageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language)
+        val animal_button: Button= findViewById(R.id.animal_button)
+        animal_button.setOnClickListener {
+            val intent= Intent(this, Animals_Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
